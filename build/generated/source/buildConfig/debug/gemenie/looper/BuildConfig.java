e7f41ff5516fabd0253369b2640512d998630359
CJ <christopher.galliart@gmail.com>
2023-05-12 13:56:46 -0500

    Added Comments

---
+++ build/generated/source/buildConfig/debug/gemenie/looper/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package gemenie.looper;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "gemenielabs.looper";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 18;
+  public static final String VERSION_NAME = "Looper";
+}
